49a8c15bf1021c0ba29dfb59cb22ef5bddcf1a9f
CDwatermelon <72157292+CDwatermelon@users.noreply.github.com>
2021-10-18 10:57:07 -0400

    Update Npc.java

--- Npc.java
+++ Npc.java
@@ -1,9 +1,6 @@
 /*
 Npc
  - NoArgsConstructor
- - Npc(String _name, String _description)
- - getName
- - getSpeech
  - setDescription(String d)
  - setName(String _name)
  - setSpeech
@@ -14,5 +11,15 @@ public class Npc
   private String name;
   private String speech;
   private String description;
-  
+ 
+  public Npc(String newName, String newSpeech){
+   this.name = newName;
+   this.speech = newSpeech;
+  }
+  public String getName(){
+    return this.name;
+ }
+  public String getSpeech(){
+    return this.speech;
+ }
 }
