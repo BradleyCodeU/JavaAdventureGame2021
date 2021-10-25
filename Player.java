2f75fea56e8207ed9b98e10aa43152e338a86842
Justin Riley <20hz20khz@gmail.com>
2021-10-25 11:09:37 -0400

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,38 @@
+/*
+Attributes:
+
+health
+kickStrength
+punchStrength
+backpack
+enemiesKilled
+currentRoom
+
+Methods:
+
+addEnemyKilled()
+loseHealth( int hpLost )
+pickUpItem( Item newItem )
+dropItem()
+moveToRoom( Room newRoom )
+getHealth()
+punch()
+kick()
+getItem()
+getEnemiesKilled()
+getCurrentRoom()
+*/
+public class Player
+{
+  // instance variables
+  private int health;
+  private int kickStrength;
+  private int punchStrength;
+  private Item backpack;
+  private int enemiesKilled;
+  private Room currentRoom;
+  
+  // constructor
+  
+  // methods
+}
